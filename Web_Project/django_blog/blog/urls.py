f39d3d7a4f33dd8cafc3d800231d94225c08a808
mnowak-devops <mati.nowak26@gmail.com>
2023-10-22 20:31:02 +0200

    Change in to code add class Home

--- Web_Project/django_blog/blog/urls.py
+++ Web_Project/django_blog/blog/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import Home
 urlpatterns = [
-    path('', home)
+    path('', Home.as_view())
 ]
--- Web_Project/django_blog/blog/views.py
+++ Web_Project/django_blog/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import response, HttpResponse
-
+from django.views import View
 
 # Create your views here.
+class Home(View):
+    def get(self, request):
+        return HttpResponse("Welcome to my blog")
+    def post(self, request):
+        return HttpResponse("[POST] Welcome to my blog")
 
-def home(request):
-    return HttpResponse("Welcome to my blog")
