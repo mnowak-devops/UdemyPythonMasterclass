9a33e01ebd2bb6c3650cc928f362bccbc9187d5f
mnowak-devops <mati.nowak26@gmail.com>
2023-10-23 22:36:43 +0200

    Add database to the project

--- Web_Project/django_blog/blog/urls.py
+++ Web_Project/django_blog/blog/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import Home
+from .views import Home, Article
+
 urlpatterns = [
-    path('', Home.as_view())
+    path('', Home.as_view()),
+    path('articles', Article.as_view())
 ]
--- Web_Project/django_blog/blog/views.py
+++ Web_Project/django_blog/blog/views.py
@@ -1,6 +1,11 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import response, HttpResponse
 from django.views import View
+from django.shortcuts import render
+
+from .models import ArticleModel
+
 
 # Create your views here.
 class Home(View):
@@ -9,3 +14,9 @@ class Home(View):
     def post(self, request):
         return HttpResponse("[POST] Welcome to my blog")
 
+
+class Article(View):
+    def get(self, request):
+        articles = ArticleModel.objects.all()
+
+        return render(request, "articles.html",{"articles":articles})
