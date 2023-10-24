206f511300b39f1277dd2f256bb43babec1fcf66
mnowak-devops <mati.nowak26@gmail.com>
2023-10-24 21:57:47 +0200

    Add forms to aplication.

--- Web_Project/django_blog/blog/views.py
+++ Web_Project/django_blog/blog/views.py
@@ -2,10 +2,10 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import response, HttpResponse
 from django.views import View
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from .models import ArticleModel
-
+from .forms import ArticleForm
 
 # Create your views here.
 class Home(View):
@@ -19,4 +19,11 @@ class Article(View):
     def get(self, request):
         articles = ArticleModel.objects.all()
 
-        return render(request, "articles.html",{"articles":articles})
+        return render(request, "articles.html", {"articles": articles, "form": ArticleForm()})
+
+    def post(self, request):
+        form = ArticleForm(request.POST)
+        form.instance.created_at = datetime.now(tz=timezone.utc)
+        form.save()
+
+        return redirect("/blog/articles")
