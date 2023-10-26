cdfaf08d7ea50618b9b6341034dc34ca36624d86
mnowak-devops <mati.nowak26@gmail.com>
2023-10-26 22:32:06 +0200

    Dynamic Urls.

--- Web_Project/django_blog/blog/models.py
+++ Web_Project/django_blog/blog/models.py
@@ -8,3 +8,7 @@ class ArticleModel(models.Model):
     author = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField()
+
+
+    def __str__(self):
+        return self.title
